2c89e6bb7ac7cddfa353e1ab32c3320e3ac33d71
FedorovskayaMM <ritush@bk.ru>
2016-08-08 23:27:39 +0500

    Основные кнопки
    Добавила основные кнопки и выпадающие поля, добавила заглушки для
    действий, пока не ясно как реализовать работу с таблицами

--- src/pack/mainform.java
+++ src/pack/mainform.java
@@ -17,6 +17,15 @@ public class mainform extends JFrame{
 		label1.setBounds(10, 10, 125, 24);
 		getContentPane().add(label1);
 
+		Button button1 = new Button("Добавить дисциплину");
+		button1.setBounds(467, 10, 158, 24);
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/*Создание дисциплины, скорее всего, диалог запроса имени и все*/
+			}
+		});
+		getContentPane().add(button1);
+
 		Choice choice1 = new Choice();//Список дисциплин
 		choice1.setBounds(141, 12, 307, 22);
 		choice1.addItem("Выберите дисциплину");
@@ -32,6 +41,16 @@ public class mainform extends JFrame{
 		label2.setBounds(10, 40, 125, 24);
 		getContentPane().add(label2);
 
+		Button button2 = new Button("Добавить билет");
+		button2.setBounds(467, 40, 158, 24);
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/*Создание билета, скорее всего, тоже, запрос названия билета*/
+			}
+		});
+		button2.setEnabled(false);//пока не выбрана дициплина, неактивен
+		getContentPane().add(button2);
+
 		Choice choice2 = new Choice();//список вопросов
 		choice2.setBounds(141, 42, 307, 22);
 		choice2.addItemListener(new ItemListener(){
@@ -45,6 +64,16 @@ public class mainform extends JFrame{
 		Label label3 = new Label("Список вопросов");
 		label3.setBounds(10, 70, 125, 24);
 		getContentPane().add(label3);
+
+		Button button3 = new Button("Добавить вопрос");
+		button3.setBounds(467, 70, 158, 24);
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/*Диалог создания вопроса*/
+			}
+		});
+		button3.setEnabled(false);//пока не выбран билет, неактивен
+		getContentPane().add(button3);
 
 		Choice choice3 = new Choice();
 		choice3.setBounds(141, 72, 307, 22);
