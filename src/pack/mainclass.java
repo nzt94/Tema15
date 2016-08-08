9b72c9baa4df4791f26cdd7274bcf1ed9508375d
FedorovskayaMM <ritush@bk.ru>
2016-08-08 23:04:19 +0500

    Выпадающие поля
    Добавила на форму поля выбора дисциплины, билета, вопроса

--- src/pack/mainclass.java
+++ src/pack/mainclass.java
@@ -1,8 +1,13 @@
 package pack;
-import pack.*;
 
 class mainclass {
+	private static mainform form1 = new mainform();
 	public static void main(String[] args) {
-		mainform mForm=new mainform();
+	}
+	public static mainform getForm1() {
+		return form1;
+	}
+	public static void setForm1(mainform form1) {
+		mainclass.form1 = form1;
 	}
 }
--- src/pack/mainform.java
+++ src/pack/mainform.java
@@ -1,7 +1,10 @@
 package pack;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class mainform extends JFrame{
+	private static final long serialVersionUID = 1L;
 	public mainform() {
 		super("Главная форма");
 		getContentPane().setLayout(null);
@@ -9,6 +12,48 @@ public class mainform extends JFrame{
 		setBounds(50, 50, 800, 480);
 		setExtendedState(MAXIMIZED_BOTH);
 		setVisible(true);
+
+		Label label1 = new Label("Список дисциплин");
+		label1.setBounds(10, 10, 125, 24);
+		getContentPane().add(label1);
+
+		Choice choice1 = new Choice();//Список дисциплин
+		choice1.setBounds(141, 12, 307, 22);
+		choice1.addItem("Выберите дисциплину");
+		/*Заполнение списка дисциплин из таблицы*/
+		choice1.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent e) {
+				/*выбор дисциплины и загрузка списка билетов*/
+			}
+		});
+		getContentPane().add(choice1);
+
+		Label label2 = new Label("Список билетов");
+		label2.setBounds(10, 40, 125, 24);
+		getContentPane().add(label2);
+
+		Choice choice2 = new Choice();//список вопросов
+		choice2.setBounds(141, 42, 307, 22);
+		choice2.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent e) {
+				/*выбор дисциплины и закрузка списка вопросов в билете*/
+			}
+		});
+		choice2.setEnabled(false);//пока не выбрана дициплина, неактивен
+		getContentPane().add(choice2);
 		
+		Label label3 = new Label("Список вопросов");
+		label3.setBounds(10, 70, 125, 24);
+		getContentPane().add(label3);
+
+		Choice choice3 = new Choice();
+		choice3.setBounds(141, 72, 307, 22);
+		choice3.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent e) {
+				/*выбор вопроса и отображение его в поле редактирования*/
+			}
+		});
+		choice3.setEnabled(false);//пока не выбран билет, неактивен
+		getContentPane().add(choice3);
 	}
 }
